bdfa16a82d278774c68270aa828bb3098c49b94e
geran1ka <roman.khoruzhy@gmail.com>
2023-05-03 23:06:30 +0300

    style section hero

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import './index.html';
 // new modules
+import 'swiper/scss';
+import 'swiper/scss/pagination';
 import './index.scss';
+
+const videoBj = document.querySelector('.video-bg');
+
+videoBj.innerHTML = `
+  <source src="video/video.webm" type="video/webm">
+  <source src="video/video.mp4" type="video/mp4">
+`;
