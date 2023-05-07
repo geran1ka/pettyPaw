f9e6614570c1b408121791d1fee55e21bb2452e4
geran1ka <roman.khoruzhy@gmail.com>
2023-05-07 22:49:02 +0300

    style section career

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './index.html';
 import 'swiper/scss';
 import 'swiper/scss/pagination';
 import './index.scss';
+import { slidersInit } from './modules/sliders';
 
 const videoBj = document.querySelector('.video-bg');
 
@@ -10,3 +11,33 @@ videoBj.innerHTML = `
   <source src="video/video.webm" type="video/webm">
   <source src="video/video.mp4" type="video/mp4">
 `;
+
+
+slidersInit('.about__slider', {
+  pagination: {
+    el: '.about__slider-pagination'
+  }
+})
+
+slidersInit('.career__slider', {
+  pagination: {
+    el: '.career__slider-pagination'
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 'auto',
+      spaceBetween: 20,
+      pagination: false,
+    },
+    1024: {
+      slidesPerView: 'auto',
+      spaceBetween: 26,
+      pagination: false,
+    },
+    1240: {
+      slidesPerView: 'auto',
+      spaceBetween: 30,
+      pagination: false,
+    }
+  }
+})
---
+++ src/modules/sliders.js
@@ -0,0 +1,22 @@
+import {Swiper, Pagination, Autoplay} from "swiper";
+
+const params = {
+  spaceBetween: 20,
+  loop: true,
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+  sliderPerView: 1,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  modules: [Autoplay, Pagination],
+};
+
+export const slidersInit = (selectorsSlider, newParams) => {
+  new Swiper(selectorsSlider, {
+    ...params,
+    ...newParams,
+  })
+}
