d421a8871af65a754b225d30bcce8d412f74cdb3
geran1ka <roman.khoruzhy@gmail.com>
2023-05-11 23:24:48 +0300

    location js

--- src/index.js
+++ src/index.js
@@ -6,9 +6,11 @@ import './index.scss';
 import { slidersInit } from './modules/sliders';
 import { videoBackgroundInit } from './modules/videoBackground';
 import { menuControl } from './modules/menuControl';
+import { locationHover } from './modules/locationHower';
 
 videoBackgroundInit();
 menuControl();
+locationHover();
 
 slidersInit('.about__slider', {
   pagination: {
@@ -16,25 +18,31 @@ slidersInit('.about__slider', {
   }
 })
 
+const careerImageItems = document.querySelectorAll('.career__slider-item');
+
+careerImageItems.forEach((item, index) => {
+  item.classList.add(`career__slider-item_${index % 2 ? 'even': 'odd'}`)
+})
+
 slidersInit('.career__slider', {
   pagination: {
     el: '.career__slider-pagination'
   },
   breakpoints: {
-    768: {
+    576: {
       slidesPerView: 'auto',
       spaceBetween: 20,
-      pagination: false,
+
     },
     1024: {
       slidesPerView: 'auto',
       spaceBetween: 26,
-      pagination: false,
+
     },
     1240: {
       slidesPerView: 'auto',
       spaceBetween: 30,
-      pagination: false,
+
     }
   }
 })
